0d77f97f4985f8d6efb6dad8f5f2d245a8c40149
Lin <lincarbajales@gmail.com>
2025-08-07 14:56:50 +0200

    [feature] Main menu shows on terminal

--- src/main/java/dev/lin/controllers/HomeController.java
+++ src/main/java/dev/lin/controllers/HomeController.java
@@ -1,5 +1,15 @@
 package dev.lin.controllers;
 
+import dev.lin.views.HomeView;
+
 public class HomeController {
+   
+    public HomeController() {
+        index();
+    }
     
+    public void index() {
+        HomeView.printMenu();
+    }
+
 }
